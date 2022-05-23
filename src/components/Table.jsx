da99c51a6d5cf2b8d0b517acca4af21db274ac08
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-23 09:59:14 -0300

    Add modified table components for connecting to redux state &  solving task 8

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { collectedExpenses } = this.props;
     return (
       <table>
         <tr>
@@ -15,9 +18,30 @@ class Table extends React.Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {collectedExpenses
+        // Research using map in table: https://stackoverflow.com/questions/57457547/how-to-use-map-to-create-html-table-in-react
+          .map(({ value, description, currency, method, tag, id, exchangeRates }) => (
+            <tr key={ id }>
+              <td>{ description }</td>
+              <td>{ tag }</td>
+              <td>{ method }</td>
+              <td>{ Number(value).toFixed(2) }</td>
+              <td>{ exchangeRates[currency].name.split('/') }</td>
+              <td>{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+              <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
+              <td>Real</td>
+            </tr>))}
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  collectedExpenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  collectedExpenses: propTypes.arrayOf(propTypes.shape()).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
